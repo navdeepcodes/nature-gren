cd0bdc6ff717c3fe04ddf90313b436ab62228d88
navdeeptrox-gif <navdeeptrox@gmail.com>
2026-07-23 19:00:21 +0530

    Improve favicon and SEO metadata

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1784812392067,
+    __BUILD_TIMESTAMP_MS__: 1784813125011,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
